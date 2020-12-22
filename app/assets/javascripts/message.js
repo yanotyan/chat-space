cefb5733dc0c5af410e6c617cfaae6085f518d77
yanotyan <m.parfume.y.ha7@gmail.com>
2020-12-22 20:39:50 +0900

    イベントが発火したときにAjaxを使用

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
+})
