b10a96dd5457192a3c88861fdf472b11efe11d6b
yanotyan <m.parfume.y.ha7@gmail.com>
2020-12-23 19:38:06 +0900

    返ってきたJSONをdoneメソッドで受取り、HTMLを作成しよう

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,17 +1,63 @@
 $(function(){
+
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      let html = 
+        `<div class="message">
+          <div class="MessageTop">
+            <div class="MessageTop__username">
+              ${message.user_name}
+            </div>
+            <div class="MessageTop__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="MessageMain">
+            <p class="Message__content">
+            ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html = 
+        `<div class="message">
+           <div class="MessageTop">
+             <div class="MessageTop__username">
+              ${message.user_name}
+             </div>
+             <div class="MessageTop__date">
+              ${message.created_at}
+             </div>
+            </div>
+          <div class="MessageMain">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
   $('.form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
-    console.log(this)
-    console.log(formData)
-    // $.ajax({
-    //   url: url,
-    //   type: "POST",
-    //   data: formData,
-    //   dataType: 'json',
-    //   processData: false,
-    //   contentType: false
-    // })
-  })
-})
+  
+     $.ajax({
+       url: url,
+       type: "POST",
+       data: formData,
+       dataType: 'json',
+       processData: false,
+       contentType: false
+     })
+     .done(function(data){
+      let html = buildHTML(data);
+      console.log("data")
+     })
+  });
+});
