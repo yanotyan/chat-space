ae1646426a8ad1cb3fa307ad25020a3c1b6aa588
yanotyan <m.parfume.y.ha7@gmail.com>
2020-12-23 18:40:05 +0900

    jbuilderを使用して、作成したメッセージをJSON形式で返そう

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,13 +3,15 @@ $(function(){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
+    console.log(this)
+    console.log(formData)
+    // $.ajax({
+    //   url: url,
+    //   type: "POST",
+    //   data: formData,
+    //   dataType: 'json',
+    //   processData: false,
+    //   contentType: false
+    // })
   })
 })
